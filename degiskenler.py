7b0a798e82e9f48c7ba33499252b77d767bc2b7a
Taylan Deniz Kandemir <118368707+taylandenizkandemir@users.noreply.github.com>
2022-11-16 22:37:44 +0300

    Add files via upload

---
+++ degiskenler.py
@@ -0,0 +1,23 @@
+baslik = "Haberiniz Olsun" #string
+vade = 12 #integer
+faizOrani1 = 1.47           #float
+faizOrani2 = 1.44
+faizOrani = 1.47
+
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani))
+
+mesaj = "Hoşgeldin"
+musteriAdi = "Taylan"
+musteriSoyadi = "Kandemir"
+sonucMesaj = mesaj + " " + musteriAdi + " " +musteriSoyadi + "!" 
+
+print(sonucMesaj)
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1 + sayi2)
+
+print(sonucMesaj)
